212d84b2c0fd44cdf41dfbcd7bd19ad61ea5cd63
QuangTruong87 <tkias123456@gmail.com>
2023-04-15 14:22:06 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
