b0e2839c1e9be52054e6e9b68064bb3524d4a6b3
Student <Student@tdc.edu.vn>
2023-04-15 14:23:14 +0700

    update browse

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Route::get('', [CustomAuthController::class, 'index'])->name('login');
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
 });
